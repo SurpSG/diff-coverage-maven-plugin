ac8c969a9a41d1c4ca8e0f0113904bf06024ad6d
Sergiy Gnatyuk <surpsg2012@gmail.com>
2020-06-23 22:05:02 +0300

    init simplest plugin

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # diff-coverage-maven-plugin
+
+```xml
+<build>
+    <plugins>
+        <plugin>
+            <groupId>com.sgnat</groupId>
+            <artifactId>diff-coverage-maven-plugin</artifactId>
+            <version>0.0.1-SNAPSHOT</version>
+            <executions>
+                <execution>
+                    <configuration>
+                    </configuration>
+                    <goals>
+                        <goal>diff-coverage</goal>
+                    </goals>
+                </execution>
+            </executions>
+        </plugin>
+    </plugins>
+</build>
+```
---
+++ src/main/kotlin/com/sgnat/DiffCoverageMojo.kt
@@ -0,0 +1,52 @@
+package com.sgnat
+
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugin.MojoExecutionException
+import org.apache.maven.plugins.annotations.LifecyclePhase
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.project.MavenProject
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal touch
+ *
+ * @phase process-sources
+ */
+
+@Mojo(name = "diff-coverage", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+class DiffCoverageMojo : AbstractMojo() {
+    /**
+     * Location of the file.
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    @Parameter(defaultValue = "\${project.build.directory}", required = true, readonly = true)
+    private lateinit var outputDirectory: File
+
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    @Parameter(defaultValue = "\${project}", required = true, readonly = true)
+    private lateinit var project: MavenProject
+
+    override fun execute() {
+        if (!outputDirectory.exists()) {
+            outputDirectory.mkdirs()
+        }
+        val touch = File(outputDirectory, "touch.txt")
+        try {
+            FileWriter(touch).use { it.write("touch.txt") }
+        } catch (e: IOException) {
+            throw MojoExecutionException("Error creating file $touch", e)
+        }
+        println("diff-coverage goal completed. project name: '${project.name}'")
+    }
+}
