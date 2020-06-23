e5c3d731fe2ca7acc3186afbb287e8952521ab8f
Sergiy Gnatyuk <surpsg2012@gmail.com>
2020-06-23 23:05:44 +0300

    diff-coverage POC

--- src/main/kotlin/com/sgnat/DiffCoverageMojo.kt
+++ src/main/kotlin/com/sgnat/DiffCoverageMojo.kt
@@ -1,13 +1,15 @@
 package com.sgnat
 
+import com.form.coverage.*
+import com.form.coverage.report.ReportGenerator
+import com.form.coverage.report.analyzable.AnalyzableReportFactory
+import com.form.diff.CodeUpdateInfo
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import java.io.File
-import java.io.FileWriter
-import java.io.IOException
 
 
 /**
@@ -32,11 +34,26 @@ class DiffCoverageMojo : AbstractMojo() {
         if (!outputDirectory.exists()) {
             outputDirectory.mkdirs()
         }
-        val touch = File(outputDirectory, "touch.txt")
-        try {
-            FileWriter(touch).use { it.write("touch.txt") }
-        } catch (e: IOException) {
-            throw MojoExecutionException("Error creating file $touch", e)
-        }
+        val analyzableReports = AnalyzableReportFactory().createCoverageAnalyzerFactory(
+            setOf(
+                DiffReport(
+                    outputDirectory.resolve("diffCoverage").toPath(),
+                    setOf(Report(ReportType.HTML, "")),
+                    CodeUpdateInfo(emptyMap()),
+                    Violation(true, listOf()
+                    )
+                ),
+                FullReport(
+                    outputDirectory.resolve("fullReport").toPath(),
+                    setOf(Report(ReportType.HTML, ""))
+                )
+            )
+        )
+        ReportGenerator(
+            outputDirectory.resolve("../"),
+            setOf(outputDirectory.resolve("jacoco.exec")),
+            setOf(outputDirectory.resolve("classes")),
+            setOf(outputDirectory.resolve("../src/main/kotlin"))
+        ).create(analyzableReports)
     }
 }
